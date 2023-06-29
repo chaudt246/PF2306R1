6c874324fabc8e887eda2ce1a10c3f6f704d8088
namch <chaudt246@gmail.com>
2023-06-30 01:03:07 +0700

    bai tap 13 tinh dien tich va chu vi hinh tron

---
+++ bai tap 13-tinh chu vi va dien tich hinh tron/index.js
@@ -0,0 +1,12 @@
+let C;
+let R;
+let S;
+const PI=3.14;
+
+R = parseInt(prompt("Hãy nhập bán kính hình tròn."));
+C = 2*R*PI;
+S=R*R*PI;
+
+document.write("Chu vi hình tròn là: "+C);
+document.write("</br>");
+document.write("Diện tích hình tròn là: "+S);
