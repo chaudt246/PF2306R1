47f92df59746f2c43387bf7717d6f7cf38752dbf
namch <chaudt246@gmail.com>
2023-06-30 00:51:49 +0700

    bao tap 12 hien thi ngay thang nam

---
+++ bai tap 12-hien thi ngay thang nam/index.js
@@ -0,0 +1,7 @@
+let inputDate;
+let inputMonth;
+let inputYear;
+inputDate = prompt("Hãy nhập ngày.");
+inputMonth = prompt("Hãy nhập tháng.");
+inputYear = prompt("Hãy nhập năm.");
+document.write(inputDate+"-"+inputMonth+"-"+inputYear);
